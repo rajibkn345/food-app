c5773cc12d37cf4ba2b3cb97d4657dd9e9997384
Roman Khan Rajib <rajibkn345@gmail.com>
2022-08-03 01:56:31 +0600

    initial commit

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MealItem = (props) => {
+	return (
+		<li>
+			<div>
+				<h3>{props.name}</h3>
+			</div>
+			<div></div>
+		</li>
+	);
+};
+
+export default MealItem;
